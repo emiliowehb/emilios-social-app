8e7eb9b95b6f835cba424a52843902d2c3a6ede6
Emilio Wehbe <emilio@twindots.com>
2023-03-26 18:49:54 +0300

    Added User & Post models and routes

---
+++ server/controllers/users.js
@@ -0,0 +1,68 @@
+import User from "../models/User";
+
+/* READ */
+export const getUser = async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        res.status(200).json(user);
+    } catch (error){
+        res.status(404).json({error: error.message});
+    }
+}
+
+export const getUserFriends = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(({
+            _id, firstName, lastName, occupation, location, picturePath
+        }) => {
+            return {_id, firstName, lastName, occupation, location, picturePath}
+        });
+        res.status(200).json(formattedFriends);
+    } catch (error){
+        res.status(404).json({error: error.message});
+    }
+}
+/* UPDATE */
+export const addRemoveFriend = async(req, res) => {
+    try {
+        const { id, friendId } = req.params;
+
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+        // Check whether user is friends with the friendId user
+        if(user.friends.includes(friendId)) {
+            // If they're friends, remove the friend's ID from the user's friends array & vice versa
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+        } else {
+            // If they're NOT friends, add them as friends of each other.
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(({
+            _id, firstName, lastName, occupation, location, picturePath
+        }) => {
+            return {_id, firstName, lastName, occupation, location, picturePath}
+        });
+
+        res.status(200).json(formattedFriends);
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { createPost } from './controllers/posts.js';
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -36,7 +42,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts/create", verifyToken, upload.single("picture"), createPost);
 
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* DATABASE CONFIGURATION */
 const PORT = process.env.PORT || 6001;
